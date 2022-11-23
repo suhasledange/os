1318d6f5830f0d5315b3b6a841544d4dc74920fb
Suhas Ledange <103985325+suhasledange@users.noreply.github.com>
2022-11-23 18:13:04 +0530

    Update sample.java

--- DLL/string/sample.java
+++ DLL/string/sample.java
@@ -7,12 +7,13 @@ public class sample{
 	public native String oper(String s1, String s2);
 
 	public static void main(String args[]) {
-		Scanner scanner = new Scanner(System.in);
+	
+		Scanner sc = new Scanner(System.in);
 		String s1,s2;
 		System.out.print("Enter first string: ");
-		s1 = scanner.nextLine();
+		s1 = sc.nextLine();
 		System.out.print("Enter second string: ");
-		s2 = scanner.nextLine();
+		s2 = sc.nextLine();
 		System.out.println("Result: "+new sample().oper(s1,s2));
 	}
 }
