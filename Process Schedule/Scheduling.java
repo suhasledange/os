72630530219cd4c28a5d630cfc7f5a473b793af0
Suhas Ledange <103985325+suhasledange@users.noreply.github.com>
2022-11-24 10:20:13 +0530

    Add files via upload

--- Process Schedule/Scheduling.java
+++ Process Schedule/Scheduling.java
@@ -5,9 +5,14 @@ public class Scheduling {
     public static void main(String[] args) {
 
         FCFS fcfs=new FCFS();
+        fcfs.execute();
+        
         SJF sjf=new SJF();
+	 sjf.execute();
+	
 
         PriorityNonPreemptive pr=new PriorityNonPreemptive();
+	pr.execute();
 
         RoundRobin rr=new RoundRobin();
         rr.execute();
