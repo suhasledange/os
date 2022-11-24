499dbe097471ed5ed87789195d8325e19a30cc8d
Suhas Ledange <103985325+suhasledange@users.noreply.github.com>
2022-11-24 09:37:31 +0530

    Add files via upload

--- DLL/math/sample.c
+++ DLL/math/sample.c
@@ -4,3 +4,15 @@ JNIEXPORT int JNICALL Java_sample_add(JNIEnv *env, jobject javaobj, jint num1, j
 {
 	return num1+num2;
 }
+JNIEXPORT int JNICALL Java_sample_sub(JNIEnv *env, jobject javaobj, jint num1, jint num2) 
+{
+	return num1-num2;
+}
+JNIEXPORT int JNICALL Java_sample_mul(JNIEnv *env, jobject javaobj, jint num1, jint num2) 
+{
+	return num1*num2;
+}
+JNIEXPORT int JNICALL Java_sample_div(JNIEnv *env, jobject javaobj, jint num1, jint num2) 
+{
+	return num1/num2;
+}
--- DLL/math/sample.java
+++ DLL/math/sample.java
@@ -5,6 +5,9 @@ public class sample{
 	}
 
 	public native int add(int num1, int num2);
+	public native int sub(int num1, int num2);
+	public native int mul(int num1, int num2);
+	public native int div(int num1, int num2);
 
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
@@ -13,6 +16,10 @@ public class sample{
 		num1 = scanner.nextInt();
 		System.out.print("Enter second number: ");
 		num2 = scanner.nextInt();
-		System.out.println("Result: "+new sample().add(num1,num2));
+		System.out.println("Addition : "+new sample().add(num1,num2));
+		System.out.println("Subtraction : "+new sample().sub(num1,num2));
+		System.out.println("Multiplication : "+new sample().mul(num1,num2));
+		System.out.println("Division : "+new sample().div(num1,num2));
+		
 	}
 }
